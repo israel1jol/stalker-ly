3945755647990d3d160442c68f24acb961f46f49
israel <adigun.jolly@gmail.com>
2022-02-20 18:35:04 +0100

    Change in static file

--- project2/src/Components/Posts.component.js
+++ project2/src/Components/Posts.component.js
@@ -5,8 +5,8 @@ const Posts = ({content, user}) => {
         return file
     }
 
-    const likePost = async (postId) => {
-        const res = await fetch("http://localhost:5000/api/post/updatePostLikes/"+postId, {
+    const likePost = async (post) => {
+        const res = await fetch("http://localhost:5000/api/post/updatePostLikes/"+post._id, {
             "method":"put",
             "headers":{
                 "content-type":"application/json"
@@ -37,7 +37,7 @@ const Posts = ({content, user}) => {
                                 <hr />
                                 <div className="lead text-center">{post.postBody}</div>
                                 <br />
-                                <form className="d-flex" action="/" onSubmit={() =>  likePost(post) }>
+                                <form className="d-flex" action="/profile" onSubmit={() =>  likePost(post) }>
                                         <input type="submit" className={check_like_state(post) ? "btn btn-danger" : "btn btn-success"} value={check_like_state(post) ? "unlike" : "like"}/>
                                         <div className="special-font-2 m-2">{ post.likes.length }</div> 
                                 </form>
